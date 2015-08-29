37c9dabc92fd5c36e8c7d21e93b403afded89311
k4kfh <k4kfh.radio@gmail.com>
2015-08-29 14:00:27 -0500

    Marker Commit: Milestone
    This commit contains a functional UI in most areas, and doesn't have any
    known instabilities. The notching system works, and the partially
    functional physics engine does do what can loosely be described as
    "function". This engine does not take adhesion into account, and has no
    braking, simulated gauges, etc. It is only primitive notching physics.

--- scripts/ui.js
+++ scripts/ui.js
@@ -156,7 +156,7 @@ function handleSlider(slider, whatisit) {
 
 
 //this is a function I made to make a sort of "verbose mode" for the JS toast alerts. I put in lots of Materialize.toast alerts, but when I don't want to hear everything they're annoying. So if you set debugToastMode = false, then the debug alerts cease and only the normal toasts come.
-debugToastMode = true
+debugToastMode = false
 function debugToast(toast, time) {
     if (debugToastMode == true) {
         Materialize.toast("Debug: " + toast, time)
