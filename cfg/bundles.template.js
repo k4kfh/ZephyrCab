50cd83885cd0a9b3cdc5776d368827adb727a8b6
Hampton <k4kfh.radio@gmail.com>
2017-05-03 11:14:19 -0500

    Lots of tidying and consolidation of bundles
    NOTE: All this is done in this template file. It's not done in the
    actual bundles.js, and the minor code changes required to deal with it
    are not implemented in the simulator yet.
    
    Moved .prototype.realtime.air.compressor.calc() to
    .prototype.air.compressor.calcFlow()
    
    Moved .prototype.realtime.air.reservoir.main into
    prototype.air.reservoir.main
    Removed .prototype.realtime.air.reservoir.main.dump because I am
    removing that feature
    
    Removed lineLength from brake object since it is redundant
    
    Changed .prototype.air.compressor.flowrate to
    .prototype.air.compressor.flowrateCoeff to better describe it
    
    Removed .prototype.realtime.fuel and added .prototype.fuel.status to
    make up for it
    
    Removed .prototype.realtime.grade since it's not implemented and isn't
    feasible to implement without RAILCOM hardware
    
    Changed .prototype.coefficientOf to .prototype.coeff for simplicity
    
    Removed .prototype.type redundant variable

--- cfg/bundles.template.js
+++ cfg/bundles.template.js
@@ -10,10 +10,9 @@ bundles.locomotives = {
     {
       "builder" : "EMD",
       "name" : "F7-A",
-      "type" : "locomotive", //TODO:REMOVE; REDUNDANT
       "weight" : 250000, //Weight of the locomotive in lbs
       "maxHP" : 1500, //Horsepower of the locomotive
-      "notchRPM" : [300, 362, 425, 487, 550, 613, 675, 738, 800], //TODO: consolodate these two into one object
+      "notchRPM" : [300, 362, 425, 487, 550, 613, 675, 738, 800],
       "notchMaxSpeeds" : [null, 7.5, 15, 22.5, 30, 37.5, 45, 52.5, 60],
       "engineRunning": 0, //0 or 1 - 1 is on, 0 is off
 
@@ -21,6 +20,7 @@ bundles.locomotives = {
       {
           usage : [3.5, 6.5, 14.5, 23.4, 33.3, 45.7, 59.6, 75.3, 93.1], //array in gal/hr, by notch
           capacity : 1200, //fuel tank in GAL
+          status : 1200, //updated fuel status
       },
 
       calc : //this contains functions left open to developers to implement for a given locomotive
@@ -29,7 +29,7 @@ bundles.locomotives = {
         maxSpeed : function(trainPosition){}, //sets exceedingMaxSpeed (a coefficient to nullify TE) to 0 if we're exceeding traction motor's speed capabilities
       },
 
-      air : //currently just holds static information about air equipment
+      air : //holds static and realtime data about pneumatics
       {
           reservoir :
           {
@@ -37,65 +37,51 @@ bundles.locomotives = {
               {
                   capacity : 46.7882, //capacity of the tank in cubic feet
                   leakRate : 0, //leak rate in cubic feet per 100ms
+                  currentAtmAirVolume: 6316.307,
+                  psi : {
+                      g : 0, //gauge psi, 0 means 1atm of pressure
+                      abs : 14.696, //absolute psi
+                  },
               },
           },
-          compressor : {
+          compressor :
+          {
+              //STATIC DATA
               limits : {
                   lower : 130, //This is the point at which the compressor will turn back on and fill up the air reservoir (psi)
                   upper : 140, //This is the point at which the compressor will turn off (psi)
               },
-              flowrate : 0.28, //This is cfm/rpm, derived from "255cfm @ 900rpm for an SD45" according to Mr. Al Krug
+              flowrateCoeff : 0.28, //This is cfm/rpm, derived from "255cfm @ 900rpm for an SD45" according to Mr. Al Krug
+              //REALTIME DATA
+              running : 0, //set to 0 when it's off, 1 when it's on
+              //METHODS
+              calcFlow : function(trainPosition) { //return compressor CFM
+                  /*
+                  This function should calculate and set the flow rate in CFM. You can find out any prototype property since we gave it the trainPosition. You should NOT multiply this calculation by running or engineRunning; sim.js will do that!
+                  In an F7, the compressor is driven directly by the prime mover, so this calculation will look at RPM and go from there.
+                  */
+                  var rpm = train.all[trainPosition].prototype.realtime.rpm
+                  var cfm = rpm * 0.283; //This is a figure that I got from "255cu ft/min @ 900rpm" from Al Krug.
 
+                  return cfm;
+              },
           },
       },
 
-      "realtime" : { //TODO: remove completely and consolidate redundant objects?
+      "realtime" : {
           netForce: 0, //defined to make it work later
           speed : 0, //This is defined just to be safe.
           rollingResistance : 0, //this is pretty much a constant but we keep it here just cause
           exceedingMaxSpeed : 1, //This is a boolean stored as a number by .prototype.calc.maxSpeed . If the locomotive has reached its max speed, this becomes zero to nullify the tractive effort. If not, it remains 1.
           horsepower : 0, //This is the OUTPUT HP
-          "grade" : 0, //IMPORTANT : This can be set to any grade percentage, but it should be as a percent not as a decimal. 1 for a 1% grade, NOT 0.01!
-          fuel : {
-              status : 1200, //You should set this to the max fuel capacity, or you'll be starting off with a non-full tank
-              notifiedOfEmptyTank : false,
-          },
-          air : { //TODO: consolidate into main "air" object
-              compressor : {
-                  calc : function(trainPosition) { //return compressor CFM
-                      /*
-                      This function should calculate and set the flow rate in CFM. You can find out any prototype property since we gave it the trainPosition. You should NOT multiply this calculation by running or engineRunning; sim.js will do that!
-                      In an F7, the compressor is driven directly by the prime mover, so this calculation will look at RPM and go from there.
-                      */
-                      var rpm = train.all[trainPosition].prototype.realtime.rpm
-                      var cfm = rpm * 0.283; //This is a figure that I got from "255cu ft/min @ 900rpm" from Al Krug.
-
-                      return cfm;
-                  },
-                  flowrate : {}, //this subobject will end up containing flow rates in a few different units
-                  running : 0, //This works a lot like prototype.engineRunning;
-              },
-              reservoir : {
-                  //Realtime data about the main reservoir is stored here
-                  main : {
-                      atmAirVolume: 6316.307, //should be set to capacity * 135 (because that will make the reservoir start out at 135psi)
-                      psi : {
-                          g : 0, //gauge psi, 0 means 1atm of pressure
-                          abs : 14.696, //absolute psi
-                      },
-                      dump : false, //TODO: remove; don't let the perfect spoil the good
-                  }
-              }
-          },
       },
 
-      "coefficientOf" : { //TODO: change to coeff for succinctness
+      "coeff" : {
           rollingResistance : 0.0015,
       },
 
-      brake : { //TODO: pick a horse and ride; also better documentation, maybe a mind map?
+      brake : {
           //air brake equipment information
-          lineLength : 50, //train line length, in feet, to be used with timing. TODO: do we need this, or can we just specify timing?
           latency: 100, //time it takes to propagate a signal through the car, in milliseconds
           //makes a brake reduction on ``trainPosition`` by ``psi``, and calculates the braking force resulting
           reservoirPSI: 90, //reservoir psi
